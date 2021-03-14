e63cfa4ab12d991f776126c2eafa2678a12b37a3
Srinath Rayabarapu <srinath.rayabarapu@tesco.com>
2021-03-14 10:53:05 +0530

    hystrix dashboard added and working on config server

---
+++ api-gateway/src/main/java/com/srinath/api/gateway/api/gateway/controllers/FallBackMethodController.java
@@ -0,0 +1,21 @@
+package com.srinath.api.gateway.api.gateway.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FallBackMethodController {
+
+    @GetMapping("/userServiceFallBack")
+    public String userServiceFallBackMethod(){
+        return "User service is taking longer than expected time to return the response." +
+                " Please try again later!";
+    }
+
+    @GetMapping("/departmentServiceFallBack")
+    public String departmentServiceFallBackMethod(){
+        return "Department service is taking longer than expected time to return the response." +
+                " Please try again later!";
+    }
+
+}
---
+++ config-server/src/test/java/com/srinath/config/server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srinath.config.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
